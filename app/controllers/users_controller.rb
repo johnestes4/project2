021699b7d3c212f417b17eedc0c4c2d3bda3a278
johnestes4 <johnestes4@gmail.com>
2016-08-08 14:26:01 -0400

    Fixed authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create!(user_params)
-    binding.pry
+    @user = User.new(user_params)
     if @user.save
       flash[:notice] = "You signed up successfully"
       flash[:color]= "valid"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,31 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  attr_accessor :username, :email, :password, :password_confirmation
+  before_create :create_remember_token
+  before_save :normalize_fields
+
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :username, :presence => true, :uniqueness => {case_sensitive: false}, :length => { :in => 3..20 }
   validates :email, :presence => true, :uniqueness => true, :format => {with: EMAIL_REGEX }
-  has_secure_password
+
   validates :password, presence: true, length: { minimum: 4 }
   validates :password_confirmation, presence: true
-
+  has_secure_password
 
   has_many :messages, through: :inboxes
   has_many :messages, through: :outboxes
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+  def User.hash(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+  def create_remember_token
+    remember_token = User.hash(User.new_remember_token)
+  end
+
+  def normalize_fields
+    self.email.downcase!
+  end
+
 end
